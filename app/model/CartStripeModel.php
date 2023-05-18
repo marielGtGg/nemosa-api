9730612d42bb71b0a70cc3985850d04057d85a22
Mariel Gauthier-Grégoire <mariel.gauthier.gregoire@gmail.com>
2023-05-18 14:47:19 -0400

    sends cartItems to stripe

--- app/model/CartStripeModel.php
+++ app/model/CartStripeModel.php
@@ -16,17 +16,22 @@ class CartStripeModel extends StripeModel {
     }
 
     public function checkout($params) {
+        $line_items = [];
+
+        foreach ($params['items'] as $item) {
+            $line_items[] = [
+                'price' => $item['price_id'],
+                'quantity' => $item['quantity'],
+            ];
+        }
+
         $checkoutSession = $this->stripe->checkout->sessions->create([
             'success_url' => $params['success_url'],
             'cancel_url' => $params['cancel_url'],
-            'line_items' => [
-              [
-                'price' => 'price_1MuMzaFg5qjjHaHiE2kU6Sqy',
-                'quantity' => 2,
-              ],
-            ],
+            'line_items' => $line_items,
             'mode' => 'payment',
         ]);
+
         return ['checkout_url' => $checkoutSession->url];
     }
 
