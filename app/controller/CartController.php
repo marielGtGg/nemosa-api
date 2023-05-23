1edd4245adc8a581c7e8b0d160d128f0f66ded9e
Mariel Gauthier-Grégoire <mariel.gauthier.gregoire@gmail.com>
2023-05-22 21:35:05 -0400

    cleans up code

--- app/controller/CartController.php
+++ app/controller/CartController.php
@@ -19,65 +19,6 @@ class CartController {
         return $this->cartStripeModel->checkout($post);
     }
 
-    // protected function render($view, $variables = []) {
-    //     ob_start();
-    //     extract($variables);
-    //     require $view;
-    //     $content = ob_get_clean();    
-    //     require ROOT . '/app/view/template/default.php';    
-    // }
-
-    // protected function header($page) {
-    //     $location = 'location: index.php?page=' . $page;
-    //     header($location);
-    // }
-
-    // public function index() {
-    //     $cart = $this->cart;
-    //     $this->render(ROOT . '/app/view/cart.index.php', compact('cart'));       
-    // }
-
-    // public function checkout() {
-    //     $this->render(ROOT . '/app/view/cart.checkout.php');
-    // }
-    
-    // private function add() {
-    //     if (isset($_POST['id'])) {
-    //         $product = $this->productTable->find($_POST['id']);
-    //         $quantity = (isset($_POST['quantity']) ? $_POST['quantity'] : 1);
-    //         $item = new CartItemModel($product, $quantity);  
-    //         $this->cart->addItem($item);
-    //     }
-    // }
-
-    // public function addFromIndex() {
-    //     $this->add();
-    //     $this->header('product.index');
-    // }
-
-    // public function addFromSingle() {
-    //     $id = (isset($_POST['id']) ? $_POST['id'] : null);
-    //     $this->add();
-    //     $this->header('product.single&id=' . $id);
-    // }
-
-    // public function increaseQty() {
-    //     $this->add();
-    //     $this->header('cart.index');
-    // }
-
-    // public function deleteItem() {
-    //     if (isset($_POST['id'])) {
-    //         $this->cart->deleteItem($_POST['id']);
-    //         $this->header('cart.index');
-    //     }
-    // }
-
-    // public function emptyCart() {
-    //     $this->cart->emptyCart();
-    //     $this->header('cart.index');
-    // }
-
 }
 
 
